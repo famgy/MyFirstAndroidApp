8db277628b8b921d38a03c9a33a1ed79f669eb2d
famgy <famgy@debian>
2017-08-18 17:44:39 +0800

    add listview

--- app/src/main/java/com/suninfo/emm/myfirstandroidapp/ContactActivity.java
+++ app/src/main/java/com/suninfo/emm/myfirstandroidapp/ContactActivity.java
@@ -5,8 +5,12 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class ContactActivity extends Activity {
 
@@ -14,18 +18,58 @@ public class ContactActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Toast.makeText(ContactActivity.this, "contact-onCreate", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(ContactActivity.this, "contact-onCreate", Toast.LENGTH_SHORT).show();
         //Toast.makeText(ContactActivity.this, getClass().getSimpleName(), Toast.LENGTH_SHORT).show();
 
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_contact);
 
         Intent intent = getIntent();
-        String data = intent.getStringExtra("new_contact");
+        final String newData = intent.getStringExtra("new_contact");
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.contact_third);
-        tv.setText(data);
+        String[] sList = new String[]
+                {
+                    "wangzl : 18666999991",
+                    "eangzl : 18666999992",
+                    "fangzl : 18666999993",
+                    "gangzl : 18666999994",
+                    "hangzl : 18666999996",
+                    "jangzl : 18666999999",
+                    "jangzl : 18666999999",
+                    "kangzl : 18666999999",
+                    "langzl : 18666999999",
+                    "vangzl : 18666999999",
+                    "xangzl : 18666999999",
+                    "cangzl : 18666999999",
+                    "vangzl : 18666999999",
+                    "bangzl : 18666999999",
+                    "nangzl : 18666999999",
+                    "wangzl : 18666999999",
+                    "wangzl : 18666999999",
+                    "wangzl : 18666999999",
+                    "wangzl : 18666999999",
+                    "wangzl : 18666999999",
+                    "wangzl : 18666999999",
+                    "wangzl : 18666999999",
+                    "wangzl : 18666999999",
+                    "wangzl : 18666999999",
+                    "wangzl : 18666999999",
+                    "wangzl : 18666999999",
+                    "wangzl : 18666999999",
+                    "wangzl : 18666999999",
+                    "wangzl : 18666999999",
+                    "wangzl : 18666999999",
+                    "wangzl : 18666999999",
+                    "wangzl : 18666999999",
+                    "wangzl : 18666999999",
+                    newData
+                };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                ContactActivity.this, android.R.layout.simple_list_item_1, sList);
+
+        ListView listView = (ListView) findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
     }
 
     @Override
--- app/src/main/java/com/suninfo/emm/myfirstandroidapp/LoginActivity.java
+++ app/src/main/java/com/suninfo/emm/myfirstandroidapp/LoginActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,6 +13,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        float xdpi = getResources().getDisplayMetrics().xdpi;
+        float ydpi = getResources().getDisplayMetrics().ydpi;
+
+        Toast.makeText(LoginActivity.this, xdpi+"*"+ydpi, Toast.LENGTH_SHORT).show();
 
         Button bt_login = (Button)findViewById(R.id.bt_login);
         bt_login.setOnClickListener(new View.OnClickListener() {
